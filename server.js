cb86f6e1c03a47c1ea2bb6b27573f97929afeb62
Hasan <hasanuzzaman201601@gmail.com>
2022-10-16 19:07:38 +0600

    server done

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const userRoutes = require("./routes/userRoutes.js");
+const app = express();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+dotenv.config();
+connectDB();
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  console.log("listening to port", PORT);
+});
